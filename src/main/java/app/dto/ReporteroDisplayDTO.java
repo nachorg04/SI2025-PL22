1d94a4a08f35502011e7443ab6104ecea6c55bd3
UO283914 <UO283914@GI-AN-B7-01.aulasuo.uniovi.es>
2026-03-13 13:38:41 +0100

    cambio en reporteroDisplayDTO

--- src/main/java/app/dto/ReporteroDisplayDTO.java
+++ src/main/java/app/dto/ReporteroDisplayDTO.java
@@ -1,17 +1,17 @@
 package app.dto;
-import lombok.*;
 
 public class ReporteroDisplayDTO {
 	private Integer idReportero;
 	private String nombre;
+	private String tematicas;
 
-	// Puerta para la tabla de Java
 	public Integer getIdReportero() { return idReportero; }
 	public void setIdReportero(Integer idReportero) { this.idReportero = idReportero; }
-
-	// TRUCO: Puerta secreta para que la Base de Datos meta el ID sin quejarse
 	public void setId_reportero(Integer id_reportero) { this.idReportero = id_reportero; }
 
 	public String getNombre() { return nombre; }
 	public void setNombre(String nombre) { this.nombre = nombre; }
+
+	public String getTematicas() { return tematicas; }
+	public void setTematicas(String tematicas) { this.tematicas = tematicas; }
 }
